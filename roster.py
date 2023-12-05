b87f95896ba8918be559066631ee7b233c06895a
Your Namexuehanw <xuehanw@email.unc.edu>
2023-12-04 19:24:36 -0500

    BMI

--- roster.py
+++ roster.py
@@ -1,11 +1,14 @@
 # https://goheels.com/sports/mens-basketball/roster
 import pandas as pd 
 
-
-roster = ["Cadeau", "Davis", "Bacot"]
-player ={"Last Name": roster,
+player ={"Last Name": ["Cadeau", "Davis", "Bacot"],
          "Fist Name":["Elliot", "RJ", "Armando"],
          "height":[73,72,83],
          "weight":[180,180,240]}
 data=pd.DataFrame(player)
+
+data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+
 print(data)
+
+data.to_csv("bmi.csv")
