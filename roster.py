50b769e5cef23e99791e2d24596f888455450ca3
Your Namexuehanw <xuehanw@email.unc.edu>
2023-12-12 19:57:00 -0500

    More Players

--- roster.py
+++ roster.py
@@ -1,12 +1,13 @@
 # https://goheels.com/sports/mens-basketball/roster
 import pandas as pd 
 
-player ={"Last Name": ["Cadeau", "Davis", "Bacot"],
-         "Fist Name":["Elliot", "RJ", "Armando"],
-         "height":[73,72,83],
-         "weight":[180,180,240]}
+player ={"Last Name": ["Cadeau", "Davis", "Bacot","High","Ryan","Trimble","Wojcik", "Washington","Lebo","Landry","Okonkwo"],
+         "Fist Name":["Elliot", "RJ", "Armando","Zayden","Cormac","Seth","Paxson","Jalen","Creighton","Rob","James"],
+         "height":[73,72,83,83,78, 76, 76, 73, 73, 77, 82],
+         "weight":[180,180,240,225,195, 195, 195, 230, 180, 190,240]}
 data=pd.DataFrame(player)
 
+#bmi = weight in kg / height in meters squared
 data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
 
 print(data)
